44e795f7ae4a704265a0c3bc8cb7c9eccd32aa98
Johannes Aubart <johannes.aubart@sap.com>
2018-05-23 15:06:00 +0200

    adaption for openstack

--- deploy_certmanager.sh
+++ deploy_certmanager.sh
@@ -6,7 +6,12 @@ cd ..
 
 # find position of the line '--oidc-ca-file=/etc/kubernetes/secrets/ca.crt' and save the array position of that entry within the yaml to variable
 pos=$(kubectl -n kube-system get -o template ds kube-apiserver --template='{{range $i, $elem := (index .spec.template.spec.containers 0).command}}{{if eq $elem "--oidc-ca-file=/etc/kubernetes/secrets/ca.crt"}}{{$i}}{{end}}{{end}}')
-# delete that entry
-kubectl -n kube-system patch ds kube-apiserver --type json -p='[{"op": "remove", "path": "/spec/template/spec/containers/0/command/'$pos'"}]'
+if [ $pos ]; then
+    # delete that entry
+    kubectl -n kube-system patch ds kube-apiserver --type json -p='[{"op": "remove", "path": "/spec/template/spec/containers/0/command/'$pos'"}]'
+else
+    echo "Line '--oidc-ca-file=/etc/kubernetes/secrets/ca.crt' not found in daemonset kube-apiserver."
+    echo "It was supposed to be there and be deleted by this script, but if it's not in there, that should be fine too."
+fi
 
 echo "Certmanager successfully deployed!"
