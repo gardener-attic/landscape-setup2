bd4c2860780d9dda5db28eeca501cd47713f460b
Johannes Aubart <johannes.aubart@sap.com>
2018-05-30 09:35:26 +0200

    changed docker_run.sh to directly switch to the setup folder (readme needs to be adapted)

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 
 # Run the docker container with interactive shell, cd to the mounted folder, and source the init.sh file
 # the "&& bash" keeps the interactive mode of the docker container alive
-docker run -it -v $(pwd)/..:/landscape -w /landscape gardener_landscape bash -c "source ./setup/init.sh && bash"
+docker run -it -v $(pwd)/..:/landscape -w /landscape/setup gardener_landscape bash -c "source ./init.sh && bash"
